b01eed23259690837d44759c8e65a90dd6a9ec8c
Kuba Butkiewicz <jakub.butkiewicz94@gmail.com>
2019-01-31 20:46:47 +0100

    styled overview page

---
+++ src/components/DayMeals.js
@@ -0,0 +1,109 @@
+import React from "react";
+import { checkIfNan } from "../helpers/helpers";
+import styled from "styled-components";
+const ItemStyled = styled.li`
+  width: 100%;
+  background-color: ${({ theme }) => theme.colors.white};
+  color: ${({ theme }) => theme.colors.dark};
+  border-radius: 4.9rem;
+  :not(:last-child) {
+    margin-bottom: 3rem;
+  }
+`;
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-wrap: wrap;
+  width: 100%;
+`;
+const NutrimentsWrapper = styled.div`
+  width: 50%;
+  display: flex;
+  justify-content: space-between;
+`;
+const NutrimentText = styled.p`
+  font-size: 1.2rem;
+`;
+const ButtonStyled = styled.button`
+  background-color: ${({ theme }) => theme.colors.light};
+  border: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem;
+  margin: 0.7rem;
+  font-size: 1.5rem;
+  cursor: pointer;
+  border-radius: 0.5rem;
+`;
+const MealsList = styled.ul`
+  display: block;
+`;
+const MealsListItem = styled.li`
+  display: flex;
+  justify-content: space-between;
+  width: 70%;
+  margin: 0 auto;
+`;
+const dayNutriments = meals => {
+  let kcal = 0;
+  let proteins = 0;
+  let fat = 0;
+  let carbo = 0;
+  meals.forEach(meal => {
+    kcal += checkIfNan(meal.nutrients.ENERC_KCAL);
+    proteins += checkIfNan(meal.nutrients.PROCNT);
+    fat += checkIfNan(meal.nutrients.FAT);
+    carbo += checkIfNan(meal.nutrients.CHOCDF);
+  });
+  return {
+    kcal,
+    proteins,
+    fat,
+    carbo
+  };
+};
+
+const DayMeals = ({ dayData }) => {
+  console.log(dayData.meals);
+  return (
+    <ItemStyled>
+      <Wrapper>
+        <h3>Date: {dayData.date}</h3>
+        <NutrimentsWrapper>
+          <NutrimentText>
+            kcal: {dayNutriments(dayData.meals).kcal}{" "}
+          </NutrimentText>
+          <NutrimentText>
+            proteins: {dayNutriments(dayData.meals).proteins}{" "}
+          </NutrimentText>
+          <NutrimentText>fat: {dayNutriments(dayData.meals).fat}</NutrimentText>
+          <NutrimentText>
+            carbo: {dayNutriments(dayData.meals).carbo}
+          </NutrimentText>
+        </NutrimentsWrapper>
+        <ButtonStyled>
+          Details
+          <i className="fas fa-angle-down" />
+        </ButtonStyled>
+      </Wrapper>
+      <MealsList>
+        {dayData.meals.map(meal => {
+          return (
+            <MealsListItem key={meal.id}>
+              <p>Meal{meal.label}</p>
+              <p>kcal: {checkIfNan(meal.nutrients.ENERC_KCAL)}</p>
+              <p>proteins: {checkIfNan(meal.nutrients.PROCNT)}</p>
+              <p>fat: {checkIfNan(meal.nutrients.FAT)}</p>
+              <p>carbo: {checkIfNan(meal.nutrients.CHOCDF)}</p>
+            </MealsListItem>
+          );
+        })}
+      </MealsList>
+    </ItemStyled>
+  );
+};
+
+export default DayMeals;
--- src/components/ViewMeals.js
+++ src/components/ViewMeals.js
@@ -1,17 +1,53 @@
 import React, { Component } from "react";
+import DayMeals from "./DayMeals";
+import styled, { ThemeProvider, createGlobalStyle } from "styled-components";
+import { theme } from "../helpers/theme";
+const DaysMealList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 800px;
+  margin: 0 auto;
+`;
+const Title = styled.h1`
+  text-align: center;
+  font-size: 5rem;
+`;
+const GlobalStyle = createGlobalStyle`
+  html {
+    font-size: 62.5%;
+  }
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: "Montserrat";
+    background-color: ${({ theme }) => theme.colors.dark};
+    color: ${({ theme }) => theme.colors.white};
+  }
+  *, *::after, *::before {
+    box-sizing: border-box;
+  }
+
+  `;
+
 class ViewMeals extends Component {
   render() {
     const days = this.props.location.state.days;
-
     return (
-      <div>
-        <h1>Test</h1>
-        <ul>
-          {days.map(day => (
-            <li>{day.date}</li>
-          ))}
-        </ul>
-      </div>
+      <ThemeProvider theme={theme}>
+        <>
+          <GlobalStyle />
+          <div>
+            <Title>Nutrition Calendar</Title>
+            <DaysMealList>
+              {days.map(day => (
+                <DayMeals key={day.date} dayData={day} />
+              ))}
+            </DaysMealList>
+          </div>
+        </>
+      </ThemeProvider>
     );
   }
 }
